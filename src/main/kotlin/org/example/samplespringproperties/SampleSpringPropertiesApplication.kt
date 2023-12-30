0016088d70f971923355ed7da0546e7df595bd34
Gleb <webmaster-80@yandex.ru>
2023-12-30 14:34:34 +0300

    init project

---
+++ src/main/kotlin/org/example/samplespringproperties/SampleSpringPropertiesApplication.kt
@@ -0,0 +1,11 @@
+package org.example.samplespringproperties
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleSpringPropertiesApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleSpringPropertiesApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/samplespringproperties/SampleSpringPropertiesApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.samplespringproperties
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleSpringPropertiesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
